8bbbd514045c98db6c5cc0a3c9e724c584c3d131
Ho Yin SHIH <hyshih@connect.ust.hk>
2019-03-19 18:40:13 +0800

    Testing: Second commit for Controller

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -79,9 +79,6 @@ public class Controller {
     @FXML
     void findInstructorSfq() {
     	buttonInstructorSfq.setDisable(true);
-    	// Add line begin
-    	textAreaConsole.setText(textAreaConsole.getText() + "\n" + textfieldSfqUrl.getText());
-    	// Add line end
     }
 
     @FXML
