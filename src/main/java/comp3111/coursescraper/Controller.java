e712e74524b359538c2f4c9e816b3eaf774244b5
clchanap <clchanap@csl4wk57.win2k.cs.ust.hk>
2019-03-20 14:54:02 +0800

    zz

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -79,9 +79,9 @@ public class Controller {
     @FXML
     void findInstructorSfq() {
     	buttonInstructorSfq.setDisable(true);
-    	//Add line begin
+    	// Add line begin
     	textAreaConsole.setText(textAreaConsole.getText() + "\n" + textfieldSfqUrl.getText());
-    	//Add line end
+    	// Add line end
     }
 
     @FXML
