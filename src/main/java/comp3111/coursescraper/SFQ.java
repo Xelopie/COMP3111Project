e5643e1fca8bbcbf279e171b6293d6055d9594a3
Pete <peterchan154@gmail.com>
2019-04-27 21:29:16 +0800

    added for task 6

---
+++ src/main/java/comp3111/coursescraper/SFQ.java
@@ -0,0 +1,27 @@
+package comp3111.coursescraper;
+
+public class SFQ {
+	private String title;
+	private String instructor;
+	private double score;
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public double getScore() {
+		return score;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+}
